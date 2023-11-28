133ec848c81228046cf3fc9033dd845889d05df9
TomDesprez <tomdesprez@yahoo.com>
2023-11-28 14:53:34 +0100

    commit2

--- storage/framework/views/2bdec9e14c4a211f047c3b1af74e6adc.php
+++ storage/framework/views/2bdec9e14c4a211f047c3b1af74e6adc.php
@@ -7,16 +7,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+  
+</head>
+
+
 </head>
 <body>
-    
 
-<h1>page d'acceuil tom</h1>
-<a href="#">photothèque</a>
-<a href="#">album</a>
-<a href="#">search</a>
-<a href="#">account</a>
-<a href="#">account</a>
+
+
+<style>
+
+ul{
+    display: flex;
+  
+ 
+
+    justify-content: space-between;
+    
+  
+}
+li{
+    list-style-type: none;
+    
+    
+}
+
+nav{
+    
+    width: 100%;
+    height: 100px;
+    background-color: #F5F5F7;
+   
+    
+}
+i{
+    color: black;
+    font-size: 40px;
+}
+
+
+body{
+  display: flex;
+}
+html{
+    margin: 0;
+    padding: 0;
+}
+
+
+
+
+</style>
+    
+<nav>
+    <ul>
+        <li><a href="#"><i class='bx bx-image' ></i></a></li>
+        <li><a href="#"><i class='bx bx-images' ></i></a></li>
+        <li><input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" /></li>
+        <li><a href="#"><i class='bx bx-search' ></i></a></li>
+        <li><a href="#"><i class='bx bxs-face'></i></a></li>
+      </ul> 
+</nav>
+
+
 
 </body>
 </html>
