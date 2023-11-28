0b7d8917a7e1ec0b3c5f204c105c714f2d8a5b12
LucRousseau <luc.rousseaupro@gmail.com>
2023-11-28 14:17:00 +0100

    avant branche

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,5 @@
 <h1>page d'acceuil</h1>
+<a href="#">photothèque</a>
+<a href="#">album</a>
+<a href="#">search</a>
+<a href="#">account</a>
--- storage/framework/views/9e098c09d63333472fa33d180e59db12.php
+++ storage/framework/views/9e098c09d63333472fa33d180e59db12.php
@@ -1,1 +1,5 @@
-<h1>page d'acceuil</h1><?php /**PATH /Users/lucrousseau/Documents/_documents/PRO/DUTMMI/S3P2/tpDevBack/tp2LaravelProjet/resources/views/index.blade.php ENDPATH**/ ?>
+<h1>page d'acceuil</h1>
+<a href="#">photothèque</a>
+<a href="#">album</a>
+<a href="#">search</a>
+<a href="#">account</a><?php /**PATH /Users/lucrousseau/Documents/_documents/PRO/DUTMMI/S3P2/tpDevBack/tp2LaravelProjet/resources/views/index.blade.php ENDPATH**/ ?>
