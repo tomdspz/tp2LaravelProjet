c027d03ee69f6f0c43e73f43b3cffbfedb3fd8be
LucRousseau <luc.rousseaupro@gmail.com>
2023-12-05 15:12:50 +0100

    liaison image

--- storage/framework/views/9e098c09d63333472fa33d180e59db12.php
+++ storage/framework/views/9e098c09d63333472fa33d180e59db12.php
@@ -27,23 +27,13 @@
 
 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
-<?php $__currentLoopData = $images; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $img): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <?php echo e($img->titre); ?>
-
-    <?php echo e($img->url); ?>
-
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        <div id="exercice2">
-            <div><img src="https://images.pexels.com/photos/1050558/pexels-photo-1050558.jpeg?cs=srgb&dl=pexels-steve-johnson-1050558.jpg&fm=jpg" /></div>
-            <div><img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Photographic_lenses_front_view.jpg" /></div>
-            <div><img src="https://media.istockphoto.com/id/1187951204/photo/camera-lens-with-red-and-blue-backlight-macro-photography-lenses-horizontal-photography.jpg?s=612x612&w=0&k=20&c=CnuTkeg8vE7cvsyn7nAQB41bHZPXmEQZnZ52O7OZrtg=" /></div>
-            <div><img src="https://images.unsplash.com/photo-1582994254571-52c62d96ebab?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGVuc3xlbnwwfHwwfHx8MA%3D%3D" /></div>
-            <div><img src="https://images.pexels.com/photos/1050558/pexels-photo-1050558.jpeg?cs=srgb&dl=pexels-steve-johnson-1050558.jpg&fm=jpg" /></div>
-            <div><img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Photographic_lenses_front_view.jpg" /></div>
-            <div><img src="https://media.istockphoto.com/id/1187951204/photo/camera-lens-with-red-and-blue-backlight-macro-photography-lenses-horizontal-photography.jpg?s=612x612&w=0&k=20&c=CnuTkeg8vE7cvsyn7nAQB41bHZPXmEQZnZ52O7OZrtg=" /></div>
-            <div><img src="https://images.unsplash.com/photo-1582994254571-52c62d96ebab?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGVuc3xlbnwwfHwwfHx8MA%3D%3D" /></div>
-            <div><img src="https://media.istockphoto.com/id/1187951204/photo/camera-lens-with-red-and-blue-backlight-macro-photography-lenses-horizontal-photography.jpg?s=612x612&w=0&k=20&c=CnuTkeg8vE7cvsyn7nAQB41bHZPXmEQZnZ52O7OZrtg=" /></div>
-            <div><img src="https://images.unsplash.com/photo-1582994254571-52c62d96ebab?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGVuc3xlbnwwfHwwfHx8MA%3D%3D" /></div>
+<div id="exercice2">
+    <?php $__currentLoopData = $images; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $img): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <div><img src="<?php echo e($img->url); ?>" /></div>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</div>
+        
+            
         </div>
     </body>
 </html>
