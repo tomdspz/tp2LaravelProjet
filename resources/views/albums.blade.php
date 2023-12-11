a59a01ece53b4a6dc2a0552a95010a0ae98e3465
LucRousseau <luc.rousseaupro@gmail.com>
2023-12-11 10:15:51 +0100

    imgalbum

--- resources/views/albums.blade.php
+++ resources/views/albums.blade.php
@@ -7,14 +7,12 @@
     <div class="ensembleAlbums">
         @foreach($albums as $a)
         <div class="album">   
-
-
-           
+            <img src="https://www.foot01.com/img/images/650x600/2023/Dec/04/al-hilal-n-a-pas-besoin-de-neymar-son-coach-s-en-vante-neymar-160-368806.jpg" alt="">
             <p>{{$a->titre }}</p>
 
                 <div class="hover-album">
-                <a href="/album/{{$a->id }}">Acceder à l'album</a>
-            <p>créé le {{$a->creation}}</p>
+                    <a href="/album/{{$a->id }}">Acceder à l'album</a>
+                    <p>créé le {{$a->creation}}</p>
                 </div>
 
         </div> 
