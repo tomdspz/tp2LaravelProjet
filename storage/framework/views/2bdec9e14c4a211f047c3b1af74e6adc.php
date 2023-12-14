fade18cb638acdc813cef9c84bbae2c9b1c1cdb7
TomDesprez <tomdesprez@yahoo.com>
2023-12-14 19:48:00 +0100

    css_form

---
+++ storage/framework/views/2bdec9e14c4a211f047c3b1af74e6adc.php
@@ -0,0 +1,5 @@
+<h1>page d'acceuil</h1>
+<a href="#">photothèque</a>
+<a href="#">album</a>
+<a href="#">search</a>
+<a href="#">account</a><?php /**PATH /Users/tomdesprez/tp2LaravelProjet/resources/views/index.blade.php ENDPATH**/ ?>
