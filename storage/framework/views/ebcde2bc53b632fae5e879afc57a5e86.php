c9a844680b391fd872650154ba47f7523466f0a1
TomDesprez <tomdesprez@yahoo.com>
2023-12-14 19:26:07 +0100

    css_form

--- storage/framework/views/ebcde2bc53b632fae5e879afc57a5e86.php
+++ storage/framework/views/ebcde2bc53b632fae5e879afc57a5e86.php
@@ -29,7 +29,7 @@
         <a href="/album/<?php echo e($a->id); ?>">
             <img class ="img-album" src="https://www.foot01.com/img/images/650x600/2023/Dec/04/al-hilal-n-a-pas-besoin-de-neymar-son-coach-s-en-vante-neymar-160-368806.jpg" alt="">
             <h1 class="p-album"><?php echo e($a->titre); ?></h1>
-</a>
+    </a>
                 <div class="hover-album">
                     Acceder à l'album
                     <p class="p2-album">créé le <?php echo e($a->creation); ?></p>
