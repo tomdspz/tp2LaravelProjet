6d515ada243ba91fee6b420f2133972f0b41f69c
LucRousseau <luc.rousseaupro@gmail.com>
2023-12-17 16:01:37 +0100

    z

--- resources/views/albums.blade.php
+++ resources/views/albums.blade.php
@@ -22,20 +22,22 @@
     <div class="ensembleAlbums">
         <a href="album/create"><div class="boutonaddalbum"><span class="croi"><i class='bx bxs-layer-plus' ></i></span></div></a>
         @foreach($albums as $a)
-
         <div class="album">  
-            
+            <?php 
+            $photosalbum = DB::select('SELECT url FROM photos INNER JOIN albums ON photos.album_id = albums.id WHERE photos.album_id = ? ORDER BY photos.id LIMIT 1;', [$a->id]);
+            // echo $photosalbum[0]->url;
+            // dd($photosalbum[0]->url) 
+            // $premierephoto = $photosalbum[0]->url;
+            // echo $photosalbum[0]->url;
+            // ?>
             <a href="/album/{{$a->id }}">
                 <img class ="img-album" src="https://www.foot01.com/img/images/650x600/2023/Dec/04/al-hilal-n-a-pas-besoin-de-neymar-son-coach-s-en-vante-neymar-160-368806.jpg" style="width: 100%" style="height: 100%" alt="">
-                {{-- <img class ="img-album" src="https://png.pngtree.com/png-vector/20190504/ourlarge/pngtree-folder-icon-design-png-image_1017484.jpg" style="width: 100%" style="height: 100%" alt=""> --}}
-                {{-- <img class="img-album" src="" alt=""> --}}
                 <h1 class="p-album">{{$a->titre }}</h1>
             </a>
             <div class="hover-album">
                 Acceder à l'album
                 <p class="p2-album">créé le {{$a->creation}}</p>
             </div>
-
         </div> 
         @endforeach
     </div>
